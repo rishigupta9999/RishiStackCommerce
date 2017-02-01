0de497986f061a5821ab6920742fdbb40dea233d
Rishi Gupta <rishig@mac.com>
2017-02-01 15:55:52 -0800

    Add login test

--- test/controllers/twitter_controller_test.rb
+++ test/controllers/twitter_controller_test.rb
@@ -1,9 +1,21 @@
 require 'test_helper'
 
 class TwitterControllerTest < ActionDispatch::IntegrationTest
-  test "should get view_tweet" do
+
+  include Devise::Test::IntegrationHelpers
+  
+  test "should get view tweet page" do
+
+    @user = User.create(:email => "#{rand(50000)}@example.com")
+    sign_in @user
+
     get twitter_view_tweet_url
     assert_response :success
+  end
+
+  test "should get redirected" do
+    get twitter_view_tweet_url
+    assert_response :redirect
   end
 
 end
