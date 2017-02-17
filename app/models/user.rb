e148fe846b665a4845b6a238f116e42426586dbc
Rishi Gupta <rishig@mac.com>
2017-02-16 16:06:55 -0800

    Remove Devise capabilities for only Twitter login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
+  devise :rememberable, :trackable,
          :omniauthable, :omniauth_providers => [:twitter]
 
   def self.from_omniauth(auth)
-    byebug
 	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 	    user.provider = auth.provider
       user.uid = auth.uid
